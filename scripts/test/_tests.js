a41c00305300586b141acf2c8ac621561409c2b1
Peter Reijnders <Peter.Reijnders@verpeteren.nl>
2013-09-12 18:34:49 +0200

    got unds getHostByName working!

--- scripts/test/_tests.js
+++ scripts/test/_tests.js
@@ -1,15 +1,16 @@
 Ape.addEvent('init', function() {
 	try {
-		include('test/Systemcmd.js');
-		include('test/MySQL.js');
-		include('test/Socket.js');
-		include('test/Pipes.js');
-		include('test/Mongo.js');
+		include('test/Hostname.js');
+		//include('test/Systemcmd.js');
+		//include('test/MySQL.js');
+		//include('test/Socket.js');
+		//include('test/Pipes.js');
+		//include('test/Mongo.js');
 		//include('test/MeteorLeak.js');//this test will harm your server!!!
-		include('test/Eval.js');
-		include('test/Properties.js');
-		include('test/FileReadWrite.js');
-		include('test/Status.js');
+		//include('test/Eval.js');
+		//include('test/Properties.js');
+		//include('test/FileReadWrite.js');
+		//include('test/Status.js');
 	} catch (e) {
 		Ape.log(e.message + '\n\t' + e.fileName + ':' + e.lineNumber);
 	}
